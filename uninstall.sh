ec12cb5e79115f2bd1659ff7c7cbdcf166ecf93b
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2026-05-28 21:20:35 +0530

    working finally

--- uninstall.sh
+++ uninstall.sh
@@ -1,23 +1,8 @@
-#!/bin/sh
 
-##
-# Uninstalls Battery Toolkit and its settings.
-#
-# Copyright (C) 2022 Marvin Häuser. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-##
-
-# Remove the Battery Toolkit daemon.
-sudo rm /Library/LaunchDaemons/me.mhaeuser.batterytoolkitd.plist
-sudo rm /Library/PrivilegedHelperTools/me.mhaeuser.batterytoolkitd
-sudo launchctl remove me.mhaeuser.batterytoolkitd
-
-# Remove the Battery Toolkit daemon data.
-sudo defaults delete me.mhaeuser.batterytoolkitd
-sudo security authorizationdb remove me.mhaeuser.batterytoolkitd.manage
-
-# Remove the Battery Toolkit Autostart helper.
-launchctl remove me.mhaeuser.BatteryToolkitAutostart
-
-# Remove the Battery Toolkit app data.
-defaults remove me.mhaeuser.BatteryToolkit
+sudo rm /Library/LaunchDaemons/an.anuser.batterytoolkitd.plist
+sudo rm /Library/PrivilegedHelperTools/an.anuser.batterytoolkitd
+sudo launchctl remove an.anuser.batterytoolkitd
+sudo defaults delete an.anuser.batterytoolkitd
+sudo security authorizationdb remove an.anuser.batterytoolkitd.manage
+launchctl remove an.anuser.BatteryToolkitAutostart
+defaults remove an.anuser.BatteryToolkit
